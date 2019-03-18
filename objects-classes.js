b63e3b33b9b6619852b3abc58cecb7f19c5355b3
Sebastian Springer <info@sebastian-springer.com>
2019-03-18 13:49:26 +0100

    objects

--- objects-classes.js
+++ objects-classes.js
@@ -1,18 +1,40 @@
 // Objekte deklarieren
 const obj1 = {
-  name: "Basti",
-  age: "Too young"
+  name: 'Basti',
+  age: 'Too young',
 };
 
 const User = function() {
-  const privateVariable = "test";
+  const privateVariable = 'test';
 
   return {
-    name: "Basti",
-    age: "Too young"
+    name: 'Basti',
+    age: 'Too young',
   };
 };
 
 const user = User();
 
-const obj3 = Object.create();
+//const obj3 = Object.create();
+
+function Account(firstname, lastname) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.name = firstname[0] + lastname;
+}
+
+const basti = new Account('Basti', 'Springer');
+Account.prototype.getFullName = function() {
+  return this.firstname + ' ' + this.lastname;
+};
+
+Account.createAccount = function(firstname, lastname) {
+  return new Account(firstname, lastname);
+};
+console.log(basti instanceof Account);
+console.log(basti.getFullName());
+
+console.log(basti);
+
+const khaled = Account.createAccount('Khaled', 'Labidi');
+console.log(khaled);
