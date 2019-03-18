6416a1093e17660f00052b15f51f2aca8ce2a1e1
Khaled Labidi <khaled.labidi@maibornwolff.de>
2019-03-18 10:21:45 +0100

    Operatoren

---
+++ operators.js
@@ -0,0 +1,34 @@
+// Zuweisung
+var strName = "string";
+
+// Vergleich
+strName === "test"; // false
+
+// arithmetische Ausdrücke
+var add = 2 + 2;
+var mult = 3 * 3;
+var div = 9 / 3;
+var mod = 10 % 2;
+var exp = 4 ** 2;
+
+// post increment
+i++; // i = i + 1
+
+// pre increment
+++i;
+
+i--; // i = i - 1
+--i;
+
+// string concatenation
+var fullName = "Khaled" + " Labidi";
+
+// logische Ausdrücke
+var b = true && true;
+var b1 = true || false;
+
+var name1 = "Hans";
+var name2 = "Georg";
+
+// ternary operator
+var s1 = name1 === name2 ? "Wert1" : "Wert2";
